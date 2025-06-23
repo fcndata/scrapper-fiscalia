e8bdf1f66da8d77e6b591ad0164b468ad4bce2c4
fcd <facundo.toneguzzo@gmail.com>
2025-06-23 16:35:28 +0000

    update lambda_handler

--- src/lambda.py
+++ src/lambda.py
@@ -2,7 +2,7 @@ from trigger import main
 
 def lambda_handler(event, context):
     try:
-        main()
-        return {"statusCode": 200, "body": "Scraping ejecutado correctamente"}
+        df = main()
+        return df
     except Exception as e:
         return {"statusCode": 500, "body": str(e)}
